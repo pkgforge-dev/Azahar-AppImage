a2e09358bad8afe52712d2889ba015ba6330e8a3
Samueru-sama <xdglawyer@outlook.com>
2026-08-17 18:43:55 -0400

    preserve comment

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,5 +17,7 @@ export DEPLOY_PIPEWIRE=1
 # Deploy dependencies
 quick-sharun /usr/bin/azahar* /usr/lib/libgamemode.so*
 
+# Additional changes can be done in between here
+
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
